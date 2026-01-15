4b58c40df937f877bc70b447163af8df80169470
Trần Trung Kiên <kienhee.it@gmail.com>
2026-01-15 16:43:06 +0700

    It seems

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@
 * Template Name: Blogy
 * Template URL: https://bootstrapmade.com/blogy-bootstrap-blog-template/
 * Updated: Feb 22 2025 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
 
